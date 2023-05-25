5f0d2e9b89390a784346b0dc76f31cc54c34648b
VChouliak <vitaly.chouliak@hotmail.com>
2023-05-25 18:40:01 +0200

    Add  static layout for shop-page and app-modules

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ShopComponent } from './shop/shop.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Ski-Shop';
+export class AppComponent implements OnInit {
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+    
+  }
 }
