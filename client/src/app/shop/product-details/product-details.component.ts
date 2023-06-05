bba2b0572c30a523e9af2cdb151d04e0e17511dd
VChouliak <vitaly.chouliak@hotmail.com>
2023-06-05 14:29:23 +0200

    Adjust Layout

--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -2,25 +2,31 @@ import { Component, OnInit } from '@angular/core';
 import { IProduct } from 'src/app/shared/models/product';
 import { ShopService } from '../shop.service';
 import { ActivatedRoute } from '@angular/router';
+import { BreadcrumbService } from 'xng-breadcrumb';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.scss']
 })
-export class ProductDetailsComponent implements OnInit{
+export class ProductDetailsComponent implements OnInit {
 
   product!: IProduct;
 
-  constructor(private shopService: ShopService, private activatedRoute: ActivatedRoute){}
-  
+  constructor(private shopService: ShopService, private activatedRoute: ActivatedRoute, private breadcrumbService: BreadcrumbService) {
+    this.breadcrumbService.set('@productDetails', ' ')
+  }
+
   ngOnInit(): void {
     this.loadProduct();
   }
 
-  loadProduct(){
+  loadProduct() {
     this.shopService.getProduct(+this.activatedRoute.snapshot.paramMap.get('id')!).subscribe({
-      next: response => this.product = response,
+      next: response => {
+        this.product = response;
+        this.breadcrumbService.set('@productDetails', this.product.name);
+      },
       error: response => console.error(response)
     });
   }
